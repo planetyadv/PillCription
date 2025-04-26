5f251645c255e92240000526b6c11d9bb117b085
MICKIYEZE <ezemajklm@gmail.com>
2025-04-26 15:50:04 +0200

    Added an "About section" and "Contact Us"

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 var http = require('http');
 
-//create a server object:
 http.createServer(function (req, res) {
-  res.write('Welcome to the PillScription Website!'); //write a response to the client
-  res.end(); //end the response
-}).listen(8080); //the server object listens on port 8080
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+
+  if (req.url === '/') {
+    res.write('Welcome to the PillScription Website!');
+  } else if (req.url === '/about') {
+    res.write('About PillScription: We provide medication subscriptions.');
+  } else if (req.url === '/contact') {
+    res.write('Contact us at contact@pillscription.com');
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.write('404 Not Found');
+  }
+  
+  res.end();
+}).listen(8080);
